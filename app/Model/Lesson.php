a337acbfbbf33cc855bd891282a78babb55f3ab6
13thang08 <thangvm91@gmail.com>
2014-03-04 15:38:15 +0700

    get history 1.0

--- app/Model/Lesson.php
+++ app/Model/Lesson.php
@@ -1,9 +1,19 @@
 <?php 
 class Lesson extends AppModel{
+	//Thang viet
+	var $name = 'Lesson';
+	var $primaryKey = 'id';
+	//Het phan Thang viet
 	public $hasMany = array(
             'Test' => array(
                 'className' => 'Test'
-            )
+            ),
+            //Thang viet
+            'Bill' => array(
+            	'classname' => 'Bill',
+            	'foreignKey' => 'lesson_id'
+            	)
+            //Het phan Thang viet
         );
 /*        
         public $hasMany = array(
